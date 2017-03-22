7c24a09fda8a639813dcf73f9b847c3375696e75
Ravindra Sadaphule <rsadaphule@gmail.com>
2017-03-21 20:33:37 -0700

    added sign up form

--- src/main/java/com/devopsbuddy/config/SecurityConfig.java
+++ src/main/java/com/devopsbuddy/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.devopsbuddy.config;
 import com.devopsbuddy.backend.service.UserSecurityService;
 import com.devopsbuddy.web.controllers.ForgotMyPasswordController;
+import com.devopsbuddy.web.controllers.SignupController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -56,7 +57,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             "/console/**",
             "/h2-console/**",
             ForgotMyPasswordController.FORGOT_PASSWORD_URL_MAPPING,
-            ForgotMyPasswordController.CHANGE_PASSWORD_PATH
+            ForgotMyPasswordController.CHANGE_PASSWORD_PATH,
+            SignupController.SIGNUP_URL_MAPPING
 
     };
 
---
+++ src/main/java/com/devopsbuddy/web/controllers/SignupController.java
@@ -0,0 +1,74 @@
+package com.devopsbuddy.web.controllers;
+
+import com.devopsbuddy.backend.persistence.domain.backend.Plan;
+import com.devopsbuddy.backend.persistence.domain.backend.Role;
+import com.devopsbuddy.backend.persistence.domain.backend.User;
+import com.devopsbuddy.backend.persistence.domain.backend.UserRole;
+
+import com.devopsbuddy.backend.service.UserService;
+import com.devopsbuddy.enums.PlansEnum;
+import com.devopsbuddy.enums.RolesEnum;
+
+import com.devopsbuddy.utils.UserUtils;
+import com.devopsbuddy.web.domain.frontend.BasicAccountPayload;
+import com.devopsbuddy.web.domain.frontend.ProAccountPayload;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+import java.io.IOException;
+import java.time.Clock;
+import java.time.LocalDate;
+import java.util.*;
+
+/**
+ * Created by tedonema on 26/04/2016.
+ */
+@Controller
+public class    SignupController {
+
+
+
+    /** The application logger */
+    private static final Logger LOG = LoggerFactory.getLogger(SignupController.class);
+
+    public static final String SIGNUP_URL_MAPPING = "/signup";
+
+    public static final String PAYLOAD_MODEL_KEY_NAME = "payload";
+
+    public static final String SUBSCRIPTION_VIEW_NAME = "registration/signup";
+
+    public static final String DUPLICATED_USERNAME_KEY = "duplicatedUsername";
+
+    public static final String DUPLICATED_EMAIL_KEY = "duplicatedEmail";
+
+    public static final String SIGNED_UP_MESSAGE_KEY = "signedUp";
+
+    public static final String ERROR_MESSAGE_KEY = "message";
+
+    public static final String GENERIC_ERROR_VIEW_NAME = "error/genericError";
+
+    @RequestMapping(value = SIGNUP_URL_MAPPING, method = RequestMethod.GET)
+    public String signupGet(@RequestParam("planId") int planId, ModelMap model) {
+
+        if (planId != PlansEnum.BASIC.getId() && planId != PlansEnum.PRO.getId()) {
+            throw new IllegalArgumentException("Plan id is not valid");
+        }
+        model.addAttribute(PAYLOAD_MODEL_KEY_NAME, new ProAccountPayload());
+
+        return SUBSCRIPTION_VIEW_NAME;
+    }
+
+
+}
